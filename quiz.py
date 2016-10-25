580f5c2fdd1d12f8ed0964a69e398c7253199619
jeremiah <jeremiahrichter@gmail.com>
2016-10-25 16:33:04 -0500

    answer Cumulative Recap!

--- quiz.py
+++ quiz.py
@@ -8,25 +8,25 @@
 
 import re
 
-regexp = r""
+regexp = r"[0-9]+(?:-?[0-9]+)*"
 
 # regexp matches:
 
-#print re.findall(regexp,"123-4567") == ["123-4567"]
+print re.findall(regexp,"123-4567") == ["123-4567"]
 #>>> True
 
-#print re.findall(regexp,"1234567") == ["1234567"]
+print re.findall(regexp,"1234567") == ["1234567"]
 #>>> True
 
-#print re.findall(regexp,"08-78-88-88-88") == ["08-78-88-88-88"]
+print re.findall(regexp,"08-78-88-88-88") == ["08-78-88-88-88"]
 #>>> True
 
-#print re.findall(regexp,"0878888888") == ["0878888888"]
+print re.findall(regexp,"0878888888") == ["0878888888"]
 #>>> True
 
 # regexp does not match:
 
-#print re.findall(regexp,"-6") != ["-6"]
+print re.findall(regexp,"-6") != ["-6"]
 #>>> True
 
 
