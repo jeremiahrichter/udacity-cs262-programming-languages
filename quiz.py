9809df1f35ad811600a591bd73cad716dd0e61a6
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-10-25 16:07:49 -0500

    answer  FSM Interpretation

--- quiz.py
+++ quiz.py
@@ -2,18 +2,17 @@
 
 # Provide s1 and s2 that are both accepted, but s1 != s2.
 
-s1 = ""
+s1 = "bdf"
 
-s2 = ""
+s2 = "bdgbdf"
 
-
-edges = {(1,'a') : 2,
-         (1,'b') : 3,
-         (2,'c') : 4,
-         (3,'d') : 5,
-         (5,'c') : 2,
-         (5,'f') : 6,
-         (5,'g') : 1}
+edges = {(1, 'a'): 2,
+         (1, 'b'): 3,
+         (2, 'c'): 4,
+         (3, 'd'): 5,
+         (5, 'c'): 2,
+         (5, 'f'): 6,
+         (5, 'g'): 1}
 
 accepting = [6]
 
@@ -30,12 +29,11 @@ def fsmsim(string, current, edges, accepting):
         else:
             return False
 
-
-#print fsmsim(s1,1,edges,accepting)
+print fsmsim(s1, 1, edges, accepting)
 # >>> True
 
-#print fsmsim(s2,1,edges,accepting)
+print fsmsim(s2, 1, edges, accepting)
 # >>> True
 
-#print s1 != s2
+print s1 != s2
 # >>> True
