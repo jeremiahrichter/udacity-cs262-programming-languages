42f5e32a7fc3eb6d7efaf4f8ffb826617368712f
Jeremiah Richter <jeremiahrichter@gmail.com>
2016-10-25 15:20:07 -0500

    answer FSM Simulation

--- quiz.py
+++ quiz.py
@@ -1,23 +1,24 @@
 # FSM Simulation
 
-edges = {(1, 'a') : 2,
-         (2, 'a') : 2,
-         (2, '1') : 3,
-         (3, '1') : 3}
+edges = {(1, 'a'): 2,
+         (2, 'a'): 2,
+         (2, '1'): 3,
+         (3, '1'): 3}
 
 accepting = [3]
+
 
 def fsmsim(string, current, edges, accepting):
     if string == "":
         return current in accepting
     else:
         letter = string[0]
-        # QUIZ: You fill this out!
-        # Is there a valid edge?
-        # If so, take it.
-        # If not, return False.
-        # Hint: recursion.
+        nextState = edges.get((current, letter), False)
+        if nextState is not False:
+            return fsmsim(string[1:], nextState, edges, accepting)
+        else:
+            return False
 
 
-#print fsmsim("aaa111",1,edges,accepting)
+print fsmsim("aaa111", 1, edges, accepting)
 # >>> True
