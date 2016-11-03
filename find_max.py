09c1d4a1feba7a4bc575a58a7cf3041d79fe1c1b
jeremiah <jeremiahrichter@gmail.com>
2016-11-03 11:33:02 -0500

    add possible answer

--- find_max.py
+++ find_max.py
@@ -11,11 +11,28 @@
 
 # Example:
 
-l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood','Bible']
+l = ['Barbara', 'kingsolver', 'wrote', 'The', 'Poisonwood', 'Bible']
 f = len
+
 
 # Try it on your own!
 
+def find_max(l, f, prev_max_item=None, prev_max_result=None):
+    if len(l) == 0:
+        return prev_max_item
+    else:
+        max_item = prev_max_item
+        max_result = prev_max_result
+        if max_item is None:
+            max_item = l[0]
+            max_result = f(l[0])
+        else:
+            item = l[0]
+            result = f(item)
+            if result > max_result:
+                max_item = item
+                max_result = result
+        return find_max(l[1:], f, max_item, max_result)
 
 
-
+print find_max(l, f)
